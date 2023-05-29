749699a19abf3418d8a9a1ea5e0175b790c161ab
newm1n <fbsns20@gmail.com>
2023-05-29 18:45:38 +0800

    feat: add user authentication and user register & login feature via passport

--- app.js
+++ app.js
@@ -1,12 +1,42 @@
 const express = require("express");
-const app = express();
+const session = require("express-session");
+const usePassport = require("./config/passport"); // 載入設定檔，要寫在 express-session 以後
+const exphbs = require("express-handlebars");
+const methodOverride = require("method-override");
+const flash = require("connect-flash");
+
 require("./config/mongoose");
 
-const PORT = process.env.PORT;
+const app = express();
+const PORT = process.env.PORT || 3000;
+const routes = require("./routes");
 
-app.get("/", (req, res) => {
-  res.send("hello world");
+app.engine("hbs", exphbs({ defaultLayout: "main", extname: ".hbs" }));
+app.set("view engine", "hbs");
+
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+
+usePassport(app);
+app.use(flash());
+
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.isAuthenticated();
+  res.locals.user = req.user;
+  res.locals.successMsg = req.flash("successMsg");
+  res.locals.warningMsg = req.flash("warningMsg");
+  next();
 });
+
+app.use(routes);
 
 app.listen(PORT, () => {
   console.log(`App is running on http://localhost:${PORT}`);
---
+++ routes/modules/users.js
@@ -0,0 +1,57 @@
+const express = require("express");
+const router = express.Router();
+const passport = require("passport");
+
+router.get("/login", (req, res) => {
+  res.render("login");
+});
+
+// 加入 middleware，驗證 request 登入狀態
+router.post(
+  "/login",
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/users/login",
+  })
+);
+
+router.get("/register", (req, res) => {
+  res.render("register");
+});
+
+router.post("/register", (req, res) => {
+  // 取得註冊表單參數
+  const { name, email, password, confirmPassword } = req.body;
+  // 檢查使用者是否已經註冊
+  User.findOne({ email })
+    .then((user) => {
+      // 如果已經註冊：退回原本畫面
+      if (user) {
+        console.log("User already exists.");
+        res.render("register", {
+          name,
+          email,
+          password,
+          confirmPassword,
+        });
+      } else {
+        // 如果還沒註冊：寫入資料庫
+        return User.create({
+          name,
+          email,
+          password,
+        })
+          .then(() => res.redirect("/"))
+          .catch((err) => console.log(err));
+      }
+    })
+    .catch((err) => console.log(err));
+});
+
+router.get("/logout", (req, res) => {
+  req.logout();
+  req.flash("success_msg", "你已經成功登出。");
+  res.redirect("/users/login");
+});
+
+module.exports = router;
